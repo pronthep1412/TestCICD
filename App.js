6c2c1324d9435fa2c6a04aab307dce3310cc7d20
pronthep dangsam <pronthep.d@ibusiness.co.th>
2025-07-01 16:26:08 +0700

    wf9

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
             alignItems: 'center',
           }}
         >
-          <Text>123555ssหs</Text>
+          <Text>123555ssหsห</Text>
           <Text>123</Text>
         </View>
         <View
